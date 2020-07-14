f5d7c46cc25c47ebdbf666584cf051dcaf95e830
Evan Willhite <evan@fourkitchens.com>
2020-07-14 08:04:04 -0500

    move period

--- src/components/molecules/banner/banner.js
+++ src/components/molecules/banner/banner.js
@@ -4,6 +4,6 @@ import styles from './banner.module.css'
 
 export default () => (
   <div className={styles.banner}>
-    <p className={styles.bannerContent}>Not sure where to start? Four Kitchens can help you <a href="http://fourkitchens.com">build design systems with Emulsify.</a></p>
+    <p className={styles.bannerContent}>Not sure where to start? Four Kitchens can help you <a href="http://fourkitchens.com">build design systems with Emulsify</a>.</p>
   </div>
 )
