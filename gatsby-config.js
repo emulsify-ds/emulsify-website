90f71313c74001e167c8e9d8d60e6836b5abe99e
Michal Minecki <michal@mirzu.com>
2021-05-06 13:22:16 -0500

    Default works fine, pointless to change the way the variable is written

--- gatsby-config.js
+++ gatsby-config.js
@@ -93,7 +93,7 @@ module.exports = {
         // on every Gatsby route change.
         //
         // Defaults to gatsby-route-change
-        routeChangeEventName: "Gatsby_Route_Change",
+        //routeChangeEventName: "Gatsby_Route_Change",
       },
     },
   ],
