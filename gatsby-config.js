014b6a68abb5d20a7d888ece0d2da5556cbc7e42
Michal Minecki <michal@mirzu.com>
2021-05-06 13:04:36 -0500

    commented out stuff we weren't using.

--- gatsby-config.js
+++ gatsby-config.js
@@ -85,9 +85,9 @@ module.exports = {
         defaultDataLayer: { platform: "gatsby" },
   
         // Specify optional GTM environment details.
-        gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
-        gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
-        dataLayerName: "YOUR_DATA_LAYER_NAME",
+        //gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        //gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        //dataLayerName: "YOUR_DATA_LAYER_NAME",
   
         // Name of the event that is triggered
         // on every Gatsby route change.
