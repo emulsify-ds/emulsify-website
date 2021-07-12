dea6db248966ab4f3a9ae6cdfcc002d088ba6579
Brian Lewis <brian@fourkitchens.com>
2021-07-12 10:55:38 -0500

    feat(em-xx): installed storybook, and created initial story for banner

---
+++ .storybook/preview.js
@@ -0,0 +1,12 @@
+// Global CSS
+import '../src/components/base/base.css'
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+}
---
+++ src/components/molecules/banner/banner.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Story } from '@storybook/react'
+
+import Banner from './banner'
+
+export default {
+  title: 'Molecule/Banner',
+  component: 'Banner',
+}
+
+export const banner: Story = () => <Banner />
