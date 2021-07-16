6271629b3357a15b98531108962320e8d158c37e
Brian Lewis <brian@fourkitchens.com>
2021-07-16 15:32:01 -0500

    style(emul-11): wip - hero

--- src/components/molecules/hero/hero.stories.tsx
+++ src/components/molecules/hero/hero.stories.tsx
@@ -1,13 +1,26 @@
 import React from 'react'
 import { Story } from '@storybook/react'
 
-import { Hero } from './Hero'
+import { Hero, HeroProps } from './Hero'
 
 export default {
   title: 'Molecule/Hero',
   component: 'Hero',
+  argTypes: {
+    heading: {
+      control: {
+        type: 'text',
+      },
+      defaultValue:
+        'Emulsify is an open-source tool for creating design systems with reusable components and clear guidelines for teams',
+    },
+  },
 }
 
-export const hero: Story = () => (
-  <Hero heading="This is a test of the hero heading" />
+type HeroStoryProps = HeroProps
+
+export const hero: Story<HeroStoryProps> = ({ heading }) => (
+  <div style={{ background: 'var(--c-blue-dark)' }}>
+    <Hero heading={heading} />
+  </div>
 )
--- src/components/molecules/hero/hero.tsx
+++ src/components/molecules/hero/hero.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react'
 
 import styles from './hero.module.css'
 
-type HeroProps = {
+export type HeroProps = {
   heading: string
 }
 
