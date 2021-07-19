683caa9e749a4e7c10d610c37e7fc336ffdf1fa5
Brian Lewis <brian@fourkitchens.com>
2021-07-19 17:37:23 -0500

    feat(emul-12): initial Callout component

---
+++ src/components/molecules/Callout/Callout.tsx
@@ -0,0 +1,27 @@
+import React, { FC, ReactNode } from 'react'
+
+import styles from './callout.module.css'
+
+export type CalloutProps = {
+  heading: string
+  text: string
+  image: ReactNode
+  layout: 'media-start' | 'media-end'
+}
+
+export const Callout: FC<CalloutProps> = ({
+  heading,
+  text,
+  image,
+  layout = 'media-start',
+}) => {
+  return (
+    <div className={styles.callout} data-callout-layout={layout}>
+      <div className={styles.calloutContent}>
+        <div className={styles.calloutHeading}>{heading}</div>
+        <div className={styles.calloutText}>{text}</div>
+      </div>
+      {image && <div className={styles.calloutImage}>{image}</div>}
+    </div>
+  )
+}
