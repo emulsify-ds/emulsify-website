5bee2c69d262e246b88e63cdbdad0bef9641ad77
Brian Lewis <brian@fourkitchens.com>
2021-07-21 18:56:50 -0500

    chore(emul-15): placed Features on demo homepage

--- src/components/pages/pages.stories.tsx
+++ src/components/pages/pages.stories.tsx
@@ -7,6 +7,7 @@ import { Layout } from '../templates/Layout'
 import { CtaGrid } from '../organisms/CtaGrid/CtaGrid'
 import { Band } from '../molecules/Band/Band'
 import { Callout } from '../molecules/Callout/Callout'
+import { Features } from '../organisms/Features/Features'
 
 export default {
   title: 'Pages/Homepage',
@@ -37,6 +38,52 @@ const ctas = [
   { linkText: 'View the Latest Guide', linkUrl: '#', icon: <Arrow /> },
 ]
 
+const featuresData = [
+  {
+    heading: 'Technology & Design Leaders',
+    children: (
+      <>
+        <p>
+          Reduce development, maintenance time, and errors by reusing components
+          and via automated testing
+        </p>
+        <p>
+          Create a consistent look and feel by reusing brand-standard colors,
+          typography, and more
+        </p>
+      </>
+    ),
+  },
+  {
+    heading: 'Designers and Marketers',
+    children: (
+      <>
+        <p>Document brand and design guidelines for everyone to use</p>
+        <p>Make changes in one place and have them update everywhere</p>
+        <p>Automate accessibility testing for an accessible user experience</p>
+      </>
+    ),
+  },
+  {
+    heading: 'Developers',
+    children: (
+      <>
+        <p>
+          Automate testing to simplify prototyping and reduce development time
+        </p>
+        <p>
+          Encourage reuse across all properties to reduce errors, development
+          and maintenance efforts
+        </p>
+        <p>
+          Maintain flexibility with support for multiple development languages
+          and component customization
+        </p>
+      </>
+    ),
+  },
+]
+
 export const Homepage: Story<HomepageProps> = ({ pageTitle, location }) => (
   <Layout
     location={location}
@@ -55,6 +102,9 @@ export const Homepage: Story<HomepageProps> = ({ pageTitle, location }) => (
         text="Emulsify doesn’t just conveniently join your component library and style guide, but includes all of the workflow tools necessary for prototyping, testing, checking accessibility, and documenting."
         image={<img src="https://picsum.photos/579" alt="example image" />}
       />
+    </Band>
+    <Band bgColor="primary" size="medium">
+      <Features features={featuresData} />
     </Band>
   </Layout>
 )
