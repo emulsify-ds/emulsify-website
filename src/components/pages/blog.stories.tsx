80c70935d8550372fb189adab01bee4edae99153
Brian Lewis <brian@fourkitchens.com>
2021-07-28 15:23:37 -0500

    chore(emul-19): implemented AuthorInfo in example Blog

--- src/components/pages/blog.stories.tsx
+++ src/components/pages/blog.stories.tsx
@@ -5,6 +5,8 @@ import { WithSidebar, WithSidebarProps } from '../templates/WithSidebar'
 import { navItems } from '../data/navigation'
 import { BackLink } from '../atoms/BackLink/BackLink'
 import Signup from '../organisms/signup/signup'
+import { PageMeta, PageMetaProps } from '../molecules/PageMeta/PageMeta'
+import { AuthorInfo, AuthorInfoProps } from '../molecules/AuthorInfo/AuthorInfo'
 
 export default {
   title: 'Pages/Blogs',
@@ -12,36 +14,72 @@ export default {
     layout: 'fullscreen',
   },
   argTypes: {
-    pageTitle: {
-      control: {
-        type: 'text',
-      },
-      defaultValue: 'Why Developers Shouldn’t Fear a No-Code Interface',
-    },
     location: {
       control: {
         type: 'text',
       },
       defaultValue: '/blog/test',
     },
+    pageTitle: {
+      control: {
+        type: 'text',
+      },
+      defaultValue: 'Why Developers Shouldn’t Fear a No-Code Interface',
+    },
+    pageSubtitle: {
+      control: {
+        type: 'text',
+      },
+      defaultValue:
+        'For those of you who manage digital projects, your world centers upon change. From deploying new versions of website platforms to navigating all manner of software updates, the fast pace of the technology industry requires (and rewards) persistent upending of the status quo.',
+    },
+    name: {
+      control: {
+        type: 'text',
+      },
+      defaultValue: 'Who Deany',
+    },
+    date: {
+      control: {
+        type: 'text',
+      },
+      defaultValue: 'January 27, 2021',
+    },
+    withAuthorImage: {
+      control: {
+        type: 'boolean',
+      },
+      defaultValue: true,
+    },
   },
 }
 
-type HomepageProps = WithSidebarProps & {
-  pageTitle: string
-}
+type HomepageProps = WithSidebarProps &
+  PageMetaProps &
+  AuthorInfoProps & {
+    pageTitle: string
+    pageSubtitle: string
+    withAuthorImage: boolean
+  }
 
 export const IndividualBlog: Story<HomepageProps> = ({
   pageTitle,
+  pageSubtitle,
   location,
-}) => (
-  <WithSidebar
-    logoUrl="#"
-    location={location}
-    navItems={navItems}
-    heading={pageTitle}
-    sidebar={<Signup />}
-  >
-    <BackLink url="#" text="view all blog posts" />
-  </WithSidebar>
-)
+  withAuthorImage,
+  name,
+  date,
+}) => {
+  let image = null
+  if (withAuthorImage === true) {
+    image = <img src="https://picsum.photos/90" alt="example image" />
+  }
+  return (
+    <WithSidebar location={location} navItems={navItems} sidebar={<Signup />}>
+      <BackLink url="#" text="view all blog posts" />
+      <PageMeta heading={pageTitle} text={pageSubtitle}>
+        <AuthorInfo image={image} name={name} date={date} />
+      </PageMeta>
+    </WithSidebar>
+  )
+}
