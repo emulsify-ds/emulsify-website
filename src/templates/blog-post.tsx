61eeb7520a2de3cf78e511a4dc37050f6faf02dd
Brian Lewis <brian@fourkitchens.com>
2021-07-30 15:10:07 -0500

    chore(emul-21): implemented RichText on example blog-post

--- src/templates/blog-post.tsx
+++ src/templates/blog-post.tsx
@@ -14,6 +14,7 @@ import { Signup } from '../components/molecules/Signup/Signup'
 import { BackLink } from '../components/atoms/BackLink/BackLink'
 import { PageMeta } from '../components/molecules/PageMeta/PageMeta'
 import { AuthorInfo } from '../components/molecules/AuthorInfo/AuthorInfo'
+import { RichText } from '../components/atoms/RichText/RichText'
 
 class BlogPostTemplate extends React.Component<PageProps> {
   render(): ReactNode {
@@ -41,11 +42,13 @@ class BlogPostTemplate extends React.Component<PageProps> {
             date={post.publishDate}
           />
         </PageMeta>
-        <div
-          dangerouslySetInnerHTML={{
-            __html: post.body.childMarkdownRemark.html,
-          }}
-        />
+        <RichText>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: post.body.childMarkdownRemark.html,
+            }}
+          />
+        </RichText>
         {/* <div style={{ background: '#fff' }}>
         <div className="wrapper"> */
         /* <div className={styles.meta}> */}
