c3ed38079254ed2d81e326045e16c871d2cba951
Brian Lewis <brian@fourkitchens.com>
2021-08-05 06:42:45 -0500

    feat(emul-23): initial ContentCta component

--- src/components/molecules/Ctas/Cta.stories.tsx
+++ src/components/molecules/Ctas/Cta.stories.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 import { Story } from '@storybook/react'
 
 import { Cta, CtaProps } from './Cta/Cta'
+import { ContentCta, ContentCtaProps } from './ContentCta/ContentCta'
 import { Play } from '../../../img/play.inline'
+import { Video } from '../../../img/video'
 
 export default {
   title: 'Molecules/Ctas',
@@ -27,3 +29,20 @@ type CtaStoryProps = CtaProps
 export const cta: Story<CtaStoryProps> = ({ linkText, to }) => (
   <Cta icon={<Play />} linkText={linkText} to={to} />
 )
+
+export const contentCta: Story<ContentCtaProps> = ({
+  heading,
+  linkText,
+  to,
+}) => (
+  <ContentCta icon={<Video />} heading={heading} linkText={linkText} to={to} />
+)
+contentCta.argTypes = {
+  heading: {
+    control: {
+      type: 'text',
+    },
+    defaultValue:
+      'Emulsify is an open-source project that’s free for everyone.',
+  },
+}
