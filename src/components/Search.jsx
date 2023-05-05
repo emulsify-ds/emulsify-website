71299d403c3d251a9ece25ad9c9567fb4f71f4fe
Randy Oest <oest@amazingrando.com>
2023-05-05 14:01:13 -0400

    wip

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -14,14 +14,6 @@ function Hit({ hit, children }) {
   return <Link href={hit.url}>{children}</Link>
 }
 
-function SearchIcon(props) {
-  return (
-    <svg aria-hidden="true" viewBox="0 0 20 20" {...props}>
-      <path d="M16.293 17.707a1 1 0 0 0 1.414-1.414l-1.414 1.414ZM9 14a5 5 0 0 1-5-5H2a7 7 0 0 0 7 7v-2ZM4 9a5 5 0 0 1 5-5V2a7 7 0 0 0-7 7h2Zm5-5a5 5 0 0 1 5 5h2a7 7 0 0 0-7-7v2Zm8.707 12.293-3.757-3.757-1.414 1.414 3.757 3.757 1.414-1.414ZM14 9a4.98 4.98 0 0 1-1.464 3.536l1.414 1.414A6.98 6.98 0 0 0 16 9h-2Zm-1.464 3.536A4.98 4.98 0 0 1 9 14v2a6.98 6.98 0 0 0 4.95-2.05l-1.414-1.414Z" />
-    </svg>
-  )
-}
-
 export function Search() {
   const [isOpen, setIsOpen] = useState(false)
   const [modifierKey, setModifierKey] = useState()
@@ -56,19 +48,6 @@ export function Search() {
             <kbd className="font-sans">K</kbd>
           </kbd>
         )}
-        {/* className={clsx(
-              'font-width-75 text-3xl font-semibold uppercase text-white'
-            )} */}
-        {/* <SearchIcon className="h-5 w-5 flex-none fill-emulsifyBlue-300 transition-all group-hover:fill-emulsifyBlue-100 dark:fill-slate-500 " />
-        <span className="sr-only font-bold uppercase group-hover:text-emulsifyBlue-100 md:not-sr-only md:ml-2 md:text-emulsifyBlue-200 md:dark:text-slate-400">
-          Search docs
-        </span>
-        {modifierKey && (
-          <kbd className="ml-auto hidden font-medium text-emulsifyBlue-200 group-hover:text-emulsifyBlue-100 dark:text-slate-500 md:block">
-            <kbd className="font-sans">{modifierKey}</kbd>
-            <kbd className="font-sans">K</kbd>
-          </kbd>
-        )} */}
       </button>
       {isOpen &&
         createPortal(
