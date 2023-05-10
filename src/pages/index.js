bc87b5ffe11bbf08f1038cb3ba4a0459962ba7e8
Randy Oest <oest@amazingrando.com>
2023-05-10 16:35:02 -0400

    homepage wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,11 +6,16 @@ import {
   faGrid2Plus,
   faCommentCheck,
 } from '@fortawesome/pro-solid-svg-icons'
+import { faUniversalAccess } from '@fortawesome/pro-regular-svg-icons'
 import TokenIcon from '@mui/icons-material/Token'
 import Image from 'next/image'
+import ReactPlayer from 'react-player'
 import ComponentLibrary from '@/components/icons/ComponentLibrary'
 import builtWithEmulsifyImage from '../images/Built with Emulsify Stamp.png'
 import consistencyImage from '../images/home-consistency.jpg'
+import talkingDrupal from '../images/talking_drupal_logo.png'
+import togetherImage from '../images/home-together.jpg'
+import waveImage from '../images/wave.png'
 
 function Hero() {
   return (
@@ -62,7 +67,7 @@ function DesignSystemsTogether() {
     tryptch: [
       {
         icon: (
-          <ComponentLibrary className="mb-5 h-[48px] text-5xl text-emulsifyBlue-600" />
+          <ComponentLibrary className="mb-5 h-[48px] text-5xl text-orange-500" />
         ),
         title: 'Develop a component-based product experience',
         body: 'A component-based product experience allows for greater efficiency and improved quality in the design and development process.',
@@ -71,7 +76,7 @@ function DesignSystemsTogether() {
         icon: (
           <FontAwesomeIcon
             icon={faDrupal}
-            className="mb-5 text-5xl text-emulsifyBlue-600"
+            className="mb-5 text-5xl text-orange-500"
           />
         ),
         title: 'Create a fully customized Drupal theme from scratch',
@@ -80,10 +85,7 @@ function DesignSystemsTogether() {
       {
         icon: (
           <div className="text-5xl">
-            <TokenIcon
-              className="mb-5 text-emulsifyBlue-600"
-              fontSize="inherit"
-            />
+            <TokenIcon className="mb-5 text-orange-500" fontSize="inherit" />
           </div>
         ),
         title: 'Use the shared language of design tokens',
@@ -95,8 +97,9 @@ function DesignSystemsTogether() {
     <section
       className={classNames(
         'bg-gradient-to-b from-emulsifyBlue-900 to-violet-900',
-        'text-white'
+        'bg-cover text-white'
       )}
+      style={{ backgroundImage: `url("${togetherImage.src}")` }}
     >
       <div className="mx-auto max-w-8xl py-32 sm:px-2 lg:px-8 xl:px-12">
         <h2
@@ -151,7 +154,7 @@ function Consistency() {
         icon: (
           <FontAwesomeIcon
             icon={faGrid2Plus}
-            className="mb-5 text-5xl text-orange-600"
+            className="mb-5 text-5xl text-emulsifyBlue-500"
           />
         ),
         title: 'Get brand consistency, efficiently',
@@ -161,22 +164,27 @@ function Consistency() {
         icon: (
           <FontAwesomeIcon
             icon={faCommentCheck}
-            className="mb-5 text-5xl text-orange-600"
+            className="mb-5 text-5xl text-emulsifyBlue-500"
           />
         ),
-        title:
-          'Let departments and schools express themselves while keeping your brand consistent',
+        title: 'Let departments and schools express themselves',
         body: 'Give clear guidelines to departments, schools, and other entities on how to express themselves while staying true to your brand. Find a balance between individuality and consistency to maintain a cohesive brand image.',
+      },
+      {
+        icon: (
+          <FontAwesomeIcon
+            icon={faUniversalAccess}
+            className="mb-5 text-5xl text-emulsifyBlue-500"
+          />
+        ),
+        title: 'Include everyone with high accessibility standards',
+        body: 'Early identification and addressing potential accessibility issues create a more accessible experience for everyone, including users with disabilities who may struggle with inaccessible websites.',
       },
     ],
   }
   return (
     <section
-      className={classNames(
-        'bg-gradient-to-b from-emulsifyBlue-900 to-violet-900',
-        'text-white',
-        'bg-cover'
-      )}
+      className={classNames('text-white', 'bg-cover')}
       style={{ backgroundImage: `url("${consistencyImage.src}")` }}
     >
       <div className="mx-auto max-w-8xl py-32 sm:px-2 lg:px-8 xl:px-12">
@@ -188,8 +196,12 @@ function Consistency() {
         >
           Keep <em className="brand-consistency">brand consistency</em> across
           all of your websites
+          <span className="block text-5xl text-orange-200">
+            (while maintaining <FontAwesomeIcon icon={faUniversalAccess} />{' '}
+            accessibility)
+          </span>
         </h2>
-        <ul className="grid max-w-[1000px] grid-cols-2 gap-20">
+        <ul className="grid grid-cols-3 gap-14">
           {content.diptych.map((item) => (
             <li key={item.title}>
               {item.icon}
@@ -209,6 +221,56 @@ function Consistency() {
   )
 }
 
+function WhatIsEmulsify() {
+  return (
+    <section
+      className={classNames('bg-emulsifyBlue-100')}
+      style={{
+        backgroundImage: `url("${waveImage.src}")`,
+        backgroundSize: '25%',
+      }}
+    >
+      <div className="mx-auto max-w-8xl py-16 sm:px-2 lg:px-8 xl:px-12">
+        <h2
+          className={classNames(
+            'flex flex-row items-end justify-center gap-4',
+            'font-width-75 text-7xl font-black',
+            'mb-9 text-emulsifyBlue-900'
+          )}
+        >
+          What is Emulsify?
+          <Image
+            src={talkingDrupal}
+            width={169}
+            height={300}
+            alt="Talking Drupal logo"
+            className="relative -top-1"
+          />
+        </h2>
+        <div className="mx-auto max-w-4xl">
+          <ReactPlayer
+            url="https://www.youtube.com/watch?v=Yk_0fARuBas&ab_channel=TalkingDrupal"
+            controls
+            width="100%"
+            height="100%"
+            className="aspect-video"
+          />
+          <p className="mt-3">
+            Watch the full episode at{' '}
+            <a
+              href="https://www.talkingdrupal.com/340"
+              className="text-emulsifyBlue-800 underline"
+            >
+              Talking Drupal #340 - Storybook
+            </a>
+            .
+          </p>
+        </div>
+      </div>
+    </section>
+  )
+}
+
 export default function Index() {
   return (
     <div>
@@ -216,6 +278,7 @@ export default function Index() {
       <DesignSystemsTogether />
       <BuiltWithEmulsify />
       <Consistency />
+      <WhatIsEmulsify />
     </div>
   )
 }
