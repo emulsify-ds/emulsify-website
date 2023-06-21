7065dd9454bb2fea0a2ee275accb943e869b82de
Randy Oest <oest@amazingrando.com>
2023-06-20 19:47:54 -0700

    feat: add rss

---
+++ src/pages/rss.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import generateRssFeed from '../utils/generateRSSFeed'
+
+export async function getStaticProps() {
+  const posts = await generateRssFeed()
+  return { props: { posts } }
+}
+
+export default function RSS({ posts }) {
+  return (
+    <>
+      <Head>
+        <title>Emulsify RSS</title>
+        <meta property="og:title" content="Emulsify RSS" key="title" />
+      </Head>
+      <div>
+        <h1>RSS placeholder</h1>
+      </div>
+    </>
+  )
+}
