ee4e517bbc8ac0dbcc9478c9d51ff476719278df
Randy Oest <oest@amazingrando.com>
2024-08-30 17:06:09 -0400

    webinar

--- src/pages/webinars/fall-2024.js
+++ src/pages/webinars/fall-2024.js
@@ -36,55 +36,67 @@ const presenters = [
   },
 ]
 
+const Arrow = (props) => (
+  <svg viewBox="0 0 131 49" {...props}>
+    <path d="M1.48464 0.992226C2.75453 2.38653 4.30285 3.61243 5.23888 5.20003C8.4086 10.5752 13.0493 14.1658 18.4666 16.984C49.1955 32.9938 80.6358 35.268 112.898 21.9939C114.279 21.4243 115.637 20.7924 116.977 20.1244C117.993 19.6193 118.965 19.0227 120.964 17.9015C118.364 17.1807 116.709 16.5859 115 16.2796C107.465 14.9446 99.91 13.7068 92.3699 12.3996C90.3418 12.0481 87.6854 12.3939 87.6093 9.25023C87.5603 7.36993 89.2289 6.77073 92.832 6.95863C99.976 7.34023 107.128 7.58993 114.266 8.05323C117.575 8.26943 120.951 8.51323 124.127 9.38423C129.689 10.9083 131.563 14.5093 130.099 20.0616C129.592 21.9831 128.822 23.8455 128.091 25.7008C125.481 32.3562 122.437 38.875 122.279 46.236C122.264 46.896 121.38 48.138 121.107 48.08C120.265 47.909 118.995 47.369 118.845 46.741C118.345 44.6814 117.751 42.4262 118.13 40.4217C118.957 36.0298 120.355 31.7437 121.522 27.4192C121.733 26.6464 121.974 25.8833 122.418 24.389C120.842 24.9922 119.847 25.3212 118.895 25.7416C83.5569 41.2967 49.0475 39.6505 15.2673 20.9238C8.5351 17.1925 4.00481 11.7171 0.836835 4.91503C0.405335 3.98253 0.390915 2.85513 0.182495 1.81543C0.620225 1.53453 1.05381 1.25642 1.49154 0.975525L1.48464 0.992226Z" />
+  </svg>
+)
+
 const Form = () => (
   <article
     className={classNames(
       'not-prose w-full scroll-pt-28 ',
       'md:float-right md:ml-8 lg:-mr-8 md:mb-8 md:max-w-[300px] space-y-6',
       'p-7 rounded border border-solid border-emulsifyBlue-400',
-      'bg-emulsifyBlue-100 bg-gradient-to-b from-emulsifyBlue-100 to-emulsifyBlue-300',
+      'bg-gradient-to-b from-emulsifyBlue-100 to-emulsifyBlue-300',
+      'dark:bg-gradient-to-b dark:from-violet-700 dark:via-emulsifyBlue-800 dark:to-emulsifyBlue-800',
     )}
     id="register"
   >
-    <h2 className="font-width-75 text-balance text-3xl font-bold leading-none text-emulsifyBlue-800">
+    <h2 className="font-width-75 text-balance text-3xl font-bold leading-none text-emulsifyBlue-800 dark:text-white">
       Register now to secure your spot!
     </h2>
 
     <label className="block">
-      <span className="text-base font-semibold leading-none text-emulsifyBlue-900">
+      <span className="text-base font-semibold leading-none text-emulsifyBlue-900 dark:text-emulsifyBlue-100">
         Name
       </span>
       <input
         type="text"
-        className="mt-1 block w-full rounded-md border-emulsifyBlue-400 shadow-sm focus:border-violet-500 focus:ring focus:ring-violet-300"
+        className="mt-1 block w-full rounded-md border-emulsifyBlue-400 shadow-sm focus:border-violet-500 focus:ring focus:ring-violet-300 dark:text-emulsifyBlue-900"
         placeholder=""
       />
     </label>
 
     <label className="block">
-      <span className="text-base font-semibold leading-none text-emulsifyBlue-900">
+      <span className="text-base font-semibold leading-none text-emulsifyBlue-900 dark:text-emulsifyBlue-100">
         Email address
       </span>
       <input
         type="email"
-        className="mt-1 block w-full rounded-md border-emulsifyBlue-400 shadow-sm focus:border-violet-500 focus:ring focus:ring-violet-300"
+        className="mt-1 block w-full rounded-md border-emulsifyBlue-400 shadow-sm focus:border-violet-500 focus:ring focus:ring-violet-300 dark:text-emulsifyBlue-900"
         placeholder="john@example.com"
       />
     </label>
 
-    <Link
-      href="/docs/emulsify-drupal"
-      className={classNames(
-        'inline-block rounded-lg',
-        'bg-violet-600 text-base font-bold text-white',
-        'transition-all hover:bg-violet-700',
-        'px-8 py-1 uppercase',
-        'focus:ring focus:ring-violet-900',
-        'focus-visible:ring focus-visible:ring-violet-900 focus-visible:border-violet-900 focus-visible:ring-offset-2',
-      )}
-    >
-      Register
-    </Link>
+    <div className="relative">
+      <Link
+        href="/docs/emulsify-drupal"
+        className={classNames(
+          'inline-block rounded-lg',
+          'bg-violet-600 text-base font-bold text-white',
+          'transition-all hover:bg-violet-700',
+          'px-8 py-1 uppercase',
+          'focus:ring focus:ring-violet-900',
+          'focus-visible:ring focus-visible:ring-violet-900 focus-visible:border-violet-900 focus-visible:ring-offset-2',
+          'dark:bg-violet-600 dark:border dark:border-solid dark:border-violet-1000',
+          'dark:hover:bg-violet-700',
+        )}
+      >
+        Register
+      </Link>
+      <Arrow className="absolute bottom-[-40px] left-[-140px] w-[150px] fill-emulsifyBlue-800 dark:fill-emulsifyBlue-100" />
+    </div>
   </article>
 )
 
